55a733f5fd584adf3f321d2ccf1cbae570c101e1
DR497 <47689875+dr497@users.noreply.github.com>
2024-01-08 15:08:21 +0800

    js: replace yarn with npm + change rollup config

---
+++ js/rollup.config.mjs
@@ -0,0 +1,36 @@
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import babel from "@rollup/plugin-babel";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.mjs",
+      format: "esm",
+      sourcemap: true,
+    },
+    { file: "dist/index.cjs", format: "cjs", sourcemap: true },
+  ],
+  external: ["@solana/web3.js"],
+  plugins: [
+    typescript({ sourceMap: true }),
+    commonjs(),
+    babel({ babelHelpers: "bundled" }),
+    json(),
+    nodeResolve({ browser: true, preferBuiltins: false }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      preventAssignment: false,
+    }),
+    terser(),
+  ],
+  onwarn: function (warning, handler) {
+    if (warning.code === "THIS_IS_UNDEFINED") return;
+    handler(warning);
+  },
+};
